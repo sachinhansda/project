c4a08cb94036db64dd41155a4d72cd705622d3c1
Sachin Hansda <sachinhansda8@gmail.com>
2020-04-25 11:27:26 +0530

    login redirect modified

--- ta_portal/accounts/urls.py
+++ ta_portal/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-#from . import views
+from . import views
 from django.contrib.auth.views import ( 
 	login, 
 	logout
 )
 
 urlpatterns = [
+	url(r'^home/$', views.home, name='home'),
     	url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     	url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name='logout')  
 ]    
--- ta_portal/accounts/views.py
+++ ta_portal/accounts/views.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	args = {}
+	return render(request, 'accounts/home.html', args)
